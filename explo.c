937a8ec0eecc3d40dbbfc1548d437706e9952e08
Mary1525 <mariefrey25@gmail.com>
2020-02-06 16:13:24 +0100

    Update explo.c

--- explo.c
+++ explo.c
@@ -3,29 +3,57 @@
 #include <time.h>
 #include <string.h>
 
+struct Lieu{
+	char nom[50];
+	char description[500];
+	int difficult;
+	int tID[10];
+};
+
+typedef struct Lieu lieu;
+
+void deplacements( lieu tLieu[],int destinationChoice){
+	
+	
+	
+	if(destinationChoice==1){
+		
+	printf ("\n La Ville de %s, %s , cette ville a un niveau de danger de %d/19\n",tLieu[0].nom,tLieu[0].description,tLieu[0].difficult);
+	
+	}
+	
+	if(destinationChoice==2){
+		
+	printf ("\n La Ville de %s, %s , cette ville a un niveau de danger de %d/19\n",tLieu[1].nom,tLieu[1].description,tLieu[1].difficult);
+	
+	}
+	
+	if(destinationChoice==3){
+	
+	printf ("\n La Ville de %s, %s , cette ville a un niveau de danger de %d/19\n",tLieu[2].nom,tLieu[2].description,tLieu[2].difficult);
+	
+	}
+}
+
+
 int main(){
-	char choixDestination[100];
-	char destination1[100]="Paris";
-	char destination2[100]="Barcelone";
-	int destinationChoice=0;
+	
+	int destinationChoice;
+	
+	lieu lieu0 = {"Somali","Cette ville connait la famine est presente",6,1};
+    lieu lieu1 = {"Hyrule","Il y a des colonies de gobelins qui peuplent la ville",12,{0,2}};
+    lieu lieu2 = {"Wuhan","Dans cette ville un virus mortel est vehicule dans l'air",19,1};
+
+    lieu tLieu[10] = {lieu0,lieu1,lieu2};
+	
+	printf ("\nQuelle destination veux tu choisir ?\n");
+	printf ("1)%s  2)%s  3)%s \n",tLieu[0].nom,tLieu[1].nom,tLieu[2].nom);
+	
+	scanf("%d",&destinationChoice);
+	
+	deplacements(tLieu,destinationChoice);
 	
 	
-	printf ("\nQuelle destination veux tu choisir ? 1)%s et 2)%s\n",destination1,destination2);
-	
-	for (int i=0; i<100; i++){
-		
-	 scanf("%s",&choixDestination);
-	
-		if (strcmp(choixDestination, destination1) ==0) {
-			printf( "Vous avez atteint %s", destination1);
-		}
-		
-		else if (strcmp(choixDestination, destination2) ==0) {
-		printf( "Vous avez atteint %s", destination2);
-		}
-			
-		};
-		
 		return 0;
 	}
 	
